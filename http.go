64ebcdb23329c41877c3699f727a1dae68d1e5e8
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-09-10 15:00:12 +0200

    http: Parse URL for uniform behavior

--- http.go
+++ http.go
@@ -4,20 +4,36 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 type HTTPBucket struct {
 	Client *http.Client
+	URL    *url.URL
 }
 
-func NewHTTPBucket(client *http.Client) *HTTPBucket {
+func NewHTTPBucket(client *http.Client, path string) (*HTTPBucket, error) {
+	u, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
 	return &HTTPBucket{
 		Client: client,
-	}
+		URL:    u,
+	}, nil
 }
 
 func (h *HTTPBucket) NewReader(ctx context.Context, path string) (io.ReadCloser, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, path, nil)
+	u := new(url.URL)
+	*u = *h.URL
+	if h.URL.User != nil {
+		u.User = new(url.Userinfo)
+		*u.User = *h.URL.User
+	}
+
+	u.Path = path
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
 	if err != nil {
 		return nil, err
 	}
