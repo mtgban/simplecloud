0cad85bb7168522442bba077d67d22f0d77249aa
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-09-10 14:09:42 +0200

    file: Add NewWriter function

--- file.go
+++ file.go
@@ -11,3 +11,7 @@ type FileBucket struct{}
 func (f *FileBucket) NewReader(ctx context.Context, path string) (io.ReadCloser, error) {
 	return os.Open(path)
 }
+
+func (f *FileBucket) NewWriter(ctx context.Context, path string) (io.WriteCloser, error) {
+	return os.Create(path)
+}
