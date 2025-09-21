cf0d81b0f239e6aed3a8c25cf62fd7e1677b47da
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-09-21 18:51:48 +0200

    Mimic the cloud behavior and create paths for file writers

--- file.go
+++ file.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 type FileBucket struct{}
@@ -13,5 +14,9 @@ func (f *FileBucket) NewReader(ctx context.Context, path string) (io.ReadCloser,
 }
 
 func (f *FileBucket) NewWriter(ctx context.Context, path string) (io.WriteCloser, error) {
+	err := os.MkdirAll(filepath.Dir(path), 0755)
+	if err != nil {
+		return nil, err
+	}
 	return os.Create(path)
 }
